9e09536f9ccfd1d8fea02c6a0879509558a59e69
Cyan Sun <62244146+NavigatorKepler@users.noreply.github.com>
2020-05-06 12:18:23 +0800

    初始化仓库

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # bilibili_api_navi
- 尝试用更便捷的方式调取bilibili的api，不准备支持任何需要登陆的功能
+ 尝试用更便捷的方式调取bilibili的api
+ 为免争议，不准备支持任何需要登陆的功能
+ 当前还在更新，而且功能并不完善
+
+# Usage / 用法
+ from bili_api import [所需要的模块]
+
+ 任何函数的返回值都应当是一个Bilibili_Response对象
+ 例如：
+    from bili_api import video_stat
+    a = video_stat(2)
+    print(a)                # <bili_api.Bilibili_Response object at 0x000001F0686B40A0>
+    print(a.data)
+    # {'av': 2, 'bvid': 'BV1xx411c7mD', 'title': '字幕君交流场所', 'category_id': 12, 'category': '', 'uploader_id': 2, 'uploader': '碧诗', 'desc': 'www', 'is_cooperation': 0, 'elec': 0, 'copyright': 2, 'no_reprint': 0, 'pubdate': 1252458549, 'view': 2111606, 'reply': 63193, 'favorite': 60215, 'coin': 21826, 'share': 8986, 'like': 102266}
+
+## Bilibili_Response
+ 一个Bilibili_Response共有六个属性
+> id:泛指uid/mid/oid等，取决于输入的id
+> level:错误等级，正常为Debug，小错误为Info，大错误为Error
+> stat:正常返回Alive，详细的会在后面讲到
+> stat_code:正常应当为0，主机端错误为-1，其他为bilibili服务器端的错误
+> type:指示data里具体的内容
+> data:实际的数据块
+
+### stat/stat_code
+> Alive 0 正常 / Unknown -1 未知 / FetchErr -1 获取失败，通常由网络阻塞导致 / AntiCrawl 412 确实中了反爬
+> Keystop -1 按键中断 / NetworkErr 网络错误
+> 仅视频：NotExist -404 视频不存在 / NeedLogin -403 视频需要登陆 / Hidden 62002 视频被隐藏
+>         Unpublish 62003 视频已过审，等待发布 / UnderReview 62004 视频审核中
+>         EmptyMap 99001 没有剧情图的互动视频 / ServerErr -500 服务器错误 / Forbidden -412 请求被拦截（可能中了反爬）
+
+## 现在支持的模块有:
+-video_stat/获取视频的常规数据
+-video_tags/获取视频的标签
+-user_info/获取用户的常规数据
+-user_videos/获取用户发布的视频
+
+## 计划支持:
+-reply/同一文章/视频/专栏下所有评论
+-rreply/某一评论下的评论，即楼中楼
+-search_video/直接搜索视频得到的结果
+-search_user/直接搜索用户得到的结果
---
+++ bili_api.py
@@ -0,0 +1,327 @@
+from time import *
+from tqdm import tqdm
+import random
+import json
+import requests
+
+def sql_replace(string):
+    string = str(string)
+    string = string.replace("'", "''")
+    # string = string.replace("\\", r"\\")
+    string = string.replace(b"\x00".decode('utf-8'), " ")
+    return string
+
+def sql_values(*args):
+    basic = "VALUES ("
+    hashead = False
+    for i in args:
+        if hashead:
+            basic += ", "
+        hashead = True
+        if i == None:
+            basic += "NULL "
+        elif isinstance(i, int):
+            basic += str(i)+" "
+        else:
+            #i = i.replace("\\", "\\\\")
+            # i = i.replace("'", "''")
+            # i = i.replace(b"\x00".decode('utf-8'), " ")
+            i = sql_replace(i)
+            basic += "'" + str(i) + "'"
+    basic += ");"
+    return basic
+
+def time_stamp(*args):
+    timestamp = args[0] if args else None
+    time = str(strftime("%Y", localtime(timestamp)) + "年" +
+               strftime("%m", localtime(timestamp)) + "月" +
+               strftime("%d", localtime(timestamp)) + "日" +
+               strftime("%H", localtime(timestamp)) + "时" +
+               strftime("%M", localtime(timestamp)) + "分" +
+               strftime("%S", localtime(timestamp)) + "秒" )
+    return time
+
+def random_head():
+    head = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+    }
+    head['Referer'] = "http://space.bilibili.com/" + \
+        str(random.randint(1, 500000000))
+    return head
+
+class Bilibili_Response(object):
+    __slots__=('id', 'stat', 'stat_code', 'type', 'level', 'data')
+
+    def __init__(self, id = None, stat = None, stat_code = None,
+                 type = None, level = None, data = None):
+        # 编号
+        # 视频为av号
+        # 用户为uid
+        # 评论为该楼的id
+        # 以此类推
+        self.id = id
+        
+        # 指示具体返回的内容
+        # video是视频信息，videolist是视频列表
+        # replylist是评论列表
+        # taglist是标签列表
+        # 以此类推
+        self.type = type
+        
+        # 这个是状态描述，不是状态码
+        self.stat = stat
+        
+        # 这个是状态码
+        self.stat_code = stat_code
+        
+        # 错误等级, debug info error三级
+        self.level = level
+        
+        # 主体部分
+        self.data = data
+
+def video_stat(av):
+    api = "http://api.bilibili.com/x/web-interface/view"
+    trytimes = 3
+    while trytimes > 0:
+        try:
+            trytimes -= 1
+            cid = random.randint(1, 140000000)
+            response = requests.get(api, params={"aid": av,"cid": cid},
+                                    headers = random_head(),
+                                    timeout = 3,
+                                    )
+            if response.status_code == 412:
+                return Bilibili_Response(id=av, type='video',
+                                         stat='AntiCrawl', stat_code=412,
+                                         level='Error', data={})
+        except KeyboardInterrupt:
+            # return("KeyStop", ())
+            return Bilibili_Response(id=av, type='video',
+                                     stat='Keystop', stat_code=-1,
+                                     level='Info', data={})
+        except Exception as e: # 网络错误
+            # return ("NetworkErr", ())
+            return Bilibili_Response(id=av, type='video',
+                                     stat='NetworkErr', stat_code=-1,
+                                     level='Error', data={})
+        main_content = json.loads(response.text)
+
+        if main_content["code"] == 0:
+            try:
+                data = main_content["data"]  # 概况部分
+                bvid = data['bvid']   # bv号，这是个字符串
+                av_num = data["aid"]  # av号
+                title = data["title"]  # 视频标题
+                category = data["tname"]  # 小分区名称
+                category_id = data["tid"]  # 小分区ID
+                copyright = data["copyright"]  # 1为原创，2为转载
+
+                owner = data["owner"]  # 投稿人信息
+                uploader = owner["name"]  # 昵称
+                uploader_id = owner["mid"]  # 投稿人mid
+                pubdate = data["pubdate"]  # 过审时间，是时间戳，未转换
+
+                rights = data["rights"]  # 权限部分
+                is_cooperation = rights["is_cooperation"]  # 是否为合作视频
+                elec = rights['elec']
+                no_reprint = rights["no_reprint"]  # 1禁止转载，0允许转载
+                desc = data["desc"]  # 简介
+
+                stat = data["stat"]  # 数据部分
+                view = stat["view"]  # 播放量
+                reply = stat["reply"]  # 回复数
+                favorite = stat["favorite"]  # 收藏数
+                coin = stat["coin"]  # 投币数
+                share = stat["share"]  # 分享数
+                like = stat["like"]  # 点赞数
+                return Bilibili_Response(id=av, type='video',
+                                     stat='Alive', stat_code=0,
+                                     level='Debug', data={'av':av_num, 'bvid':bvid,'title':title,
+                                                          'category_id':category_id, 'category':category,
+                                                          'uploader_id':uploader_id, 'uploader':uploader,
+                                                          'desc':desc,
+                                                          'is_cooperation':is_cooperation, 'elec':elec,
+                                                          'copyright':copyright, 'no_reprint':no_reprint,
+                                                          'pubdate':pubdate, 'view':view, 'reply':reply,
+                                                          'favorite':favorite, 'coin':coin, 'share':share, 'like':like})
+            except:
+                return Bilibili_Response(id=av, type='video',
+                                     stat='Unknown', stat_code=-1,
+                                     level='Error', data=main_content)
+
+        elif main_content["code"] == -404: #视频不存在
+            # return ("NotExist", ())
+            return Bilibili_Response(id=av, type='video',
+                                     stat='NotExist', stat_code=main_content["code"],
+                                     level='Info', data=main_content)
+
+        elif main_content["code"] == -403: #视频需要登陆
+            # return ("NeedLogin", ())
+            return Bilibili_Response(id=av, type='video',
+                                     stat='NeedLogin', stat_code=main_content["code"],
+                                     level='Info', data=main_content)
+
+        elif main_content["code"] == 62002: #视频不可见
+            # return ("Hidden", ())
+            return Bilibili_Response(id=av, type='video',
+                                     stat='Hidden', stat_code=main_content["code"],
+                                     level='Info', data=main_content)
+
+        elif main_content["code"] == 62003: #稿件已审核通过，等待发布中
+            # return ("UnPublish", ())
+            return Bilibili_Response(id=av, type='video',
+                                     stat='UnPublish', stat_code=main_content["code"],
+                                     level='Info', data=main_content)
+
+        elif main_content["code"] == 62004: #视频审核中
+            # return ("UnderReview", ())
+            return Bilibili_Response(id=av, type='video',
+                                     stat='UnderReview', stat_code=main_content["code"],
+                                     level='Info', data=main_content)
+
+        elif main_content['code'] == 99001:  #互动视频，但是没有剧情图
+            # return ("EmptyMap", ())
+            return Bilibili_Response(id=av, type='video',
+                                     stat='EmptyMap', stat_code=main_content["code"],
+                                     level='Info', data=main_content)
+
+        elif main_content['code'] == -500:  #服务器错误
+            # return ("ServerErr", ())
+            return Bilibili_Response(id=av, type='video',
+                                     stat='ServerErr', stat_code=main_content["code"],
+                                     level='Error', data=main_content)
+
+        elif main_content['code'] == -412:  #请求被拦截
+            return Bilibili_Response(id=av, type='video',
+                                     stat='Forbidden', stat_code=main_content["code"],
+                                     level='Error', data=main_content)
+
+        else:                               #仍然存在其他错误码
+            return Bilibili_Response(id=av, type='video',
+                                     stat='Unknown', stat_code=main_content["code"],
+                                     level='Error', data=main_content)
+
+        # tqdm.write(f"视频av{av}剩余{trytimes}次机会！")
+        sleep(1)
+    # tqdm.write(f"视频av{av}基本信息获取失败！")
+    return Bilibili_Response(id=av, type='video',
+                             stat='FetchErr', stat_code=-1,
+                             level='Error', data={})
+
+def video_tags(av):
+    api = "http://api.bilibili.com/x/tag/archive/tags"
+    trytimes = 3
+    while trytimes > 0:
+        trytimes -= 1
+        response = requests.get(api, params={"aid": av},headers = random_head())
+        if response.status_code == 412:
+            # return("AntiCrawl", ())
+            return Bilibili_Response(id=av, type='taglist',
+                                     stat='AntiCrawl', stat_code=412,
+                                     level='Error', data={})
+        main_content = json.loads(response.content)
+        if main_content["code"] == 0:
+            data = main_content["data"]
+            taglist = []
+            for i in data:
+                tag_id = i["tag_id"]
+                name = i["tag_name"]
+                tagcontent = i["content"]
+                shortcontent = i["short_content"]
+                tag = {'id':tag_id, 'tag':name, 'content':tagcontent, 'short_content':shortcontent}
+                taglist.append(tag)
+            return Bilibili_Response(id=av, type='taglist',
+                                     stat='Alive', stat_code=main_content["code"],
+                                     level='Debug', data=taglist)
+
+        elif main_content["code"] == 16001: # 该频道不存在，可能是个不正常的错误码，但查证后发现原视频没有标签 21580986
+            return Bilibili_Response(id=av, type='taglist',
+                                     stat='Alive', stat_code=main_content["code"],
+                                     level='Debug', data=main_content)
+        
+        elif main_content['code'] == -404:  # 视频不存在
+            return Bilibili_Response(id=av, type='taglist',
+                                     stat='NotExist', stat_code=main_content["code"],
+                                     level='Info', data=main_content)
+        
+        else:
+            return Bilibili_Response(id=av, type='taglist',
+                                     stat='Unknown', stat_code=main_content["code"],
+                                     level='Error', data=main_content)
+    return Bilibili_Response(id=av, type='taglist',
+                             stat='FetchErr', stat_code=-1,
+                             level='Error', data={})
+
+def user_videos(mid):       # 需要翻页，如果不sleep容易导致翻车
+    api = 'http://api.bilibili.com/x/space/arc/search'
+    trytimes = 3
+    while trytimes > 0:
+        trytimes -= 1
+        page_count = 1
+        video_list = []
+        while 1:
+            response = requests.get(api, params={'mid':mid, 'ps':20, 'pn':page_count}, headers = random_head())
+            sleep(0.1) # 为了防止过快导致封ip，sleep一下
+            if response.status_code == 412:
+                return Bilibili_Response(id=mid, type='videolist_user',
+                                        stat='AntiCrawl', stat_code=412,
+                                        level='Error', data={})
+            main_content = json.loads(response.content)
+            if main_content["code"] == 0:
+                data = main_content["data"]
+                list = data['list']
+                # tlist = list['tlist']
+                vlist = list['vlist']
+
+                if not vlist:
+                    break
+
+                for i in vlist:
+                    video = {'aid':i['aid'], 'bvid':i['bvid']}
+                    video_list.append(video)
+            else:
+                return Bilibili_Response(id=mid, type='videolist_user',
+                                        stat='Unknown', stat_code=main_content['code'],
+                                        level='Error', data=main_content)
+
+            page_count += 1
+
+        return Bilibili_Response(id=mid, type='videolist_user',
+                                        stat='Alive', stat_code=0,
+                                        level='Debug', data=video_list)
+    return Bilibili_Response(id=mid, type='videolist_user',
+                             stat='FetchErr', stat_code=-1,
+                             level='Error', data={})
+
+def user_info(mid):
+    api = 'http://api.bilibili.com/x/web-interface/card'
+    trytimes = 3
+    while trytimes > 0:
+        trytimes -= 1
+        info = {}
+        response = requests.get(api, params={'mid':mid, 'photo':'false'}, headers = random_head())
+        if response.status_code == 412:
+            return Bilibili_Response(id=mid, type='userinfo',
+                                        stat='AntiCrawl', stat_code=412,
+                                        level='Error', data={})
+        main_content = json.loads(response.content)
+        if main_content["code"] == 0:
+            data = main_content['data']['card']
+            info['mid'] = data['mid']
+            info['name'] = data['name']
+            info['fans'] = data['fans']
+            info['attention'] = data['attention']
+            info['face'] = data['face']
+            info['sign'] = data['sign']
+            info['level'] = data['level_info']['current_level']
+            return Bilibili_Response(id=mid, type='userinfo',
+                                        stat='Alive', stat_code=0,
+                                        level='Debug', data=info)
+        else:
+            return Bilibili_Response(id=mid, type='userinfo',
+                                        stat='Unknown', stat_code=main_content['code'],
+                                        level='Error', data=main_content)
+    return Bilibili_Response(id=mid, type='userinfo',
+                             stat='FetchErr', stat_code=-1,
+                             level='Error', data={})
